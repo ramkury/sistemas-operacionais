ee23ff25889aabfe63fb15f22413a517ad043556
Ricardo Kury <ricardokury@outlook.com>
2018-11-25 03:35:18 -0200

    Removes 'alloc' parameter from 'check_mem'

--- src/memory_module.py
+++ src/memory_module.py
@@ -2,33 +2,31 @@
 
 # class memory():
 # Checkagem da memória para verificar se o processo está apto a ser alocado.
-def check_mem(priority, mem_block, alloc):
-    # Caso o pedido seja de alocar processo na memória (alloc == 1)
-    if alloc == 1:
-        if priority == 0:
-            # Verifica se o espaço de memória pedido foi do tamanho correto definido de no máximo 64 blocos
-            if mem_block > 64:
-                return None
-            else:
-                # Verifica se há espaço suficiente para alocar o processo. Esta verificação se dá pela conta: addr_inicial_da_mem_livre + tamanho_restante_da_mem < espaço_pedido_pelo_processo
-                # e se addr_inicial_da_mem_livre + espaço_pedido ultrapassa o tamanho reservado na memória de 64 blocos
-                for i in range(len(rt_mem_space)):
-                    if mem_block <= int(rt_mem_space[i][1]) and int(rt_mem_space[i][0]) >= 0 and int(rt_mem_space[i][0]) <= 64 and mem_block + int(rt_mem_space[i][0]) <= 64:
-                        return alloc_mem(mem_block, i, 0)
-                
-                return None
+def check_mem(priority, mem_block):
+    if priority == 0:
+        # Verifica se o espaço de memória pedido foi do tamanho correto definido de no máximo 64 blocos
+        if mem_block > 64:
+            return None
         else:
-            # Verifica se o espaço de memória pedido foi do tamanho correto definido de no máximo 960 blocos
-            if mem_block > 960:
-                return None
-            else:
-                # Verifica se há espaço suficiente para alocar o processo. Esta verificação se dá pela conta: addr_inicial_da_mem_livre + tamanho_restante_da_mem < espaço_pedido_pelo_processo
-                # e se addr_inicial_da_mem_livre + espaço_pedido ultrapassa o tamanho reservado na memória de 960 blocos
-                for i in range(len(usr_mem_space)):
-                    if mem_block <= int(usr_mem_space[i][1]) and int(usr_mem_space[i][0]) <= 1024  and mem_block + int(usr_mem_space[i][0]) <= 1024:
-                        return alloc_mem(mem_block, i, 1)
-                
-                return None
+            # Verifica se há espaço suficiente para alocar o processo. Esta verificação se dá pela conta: addr_inicial_da_mem_livre + tamanho_restante_da_mem < espaço_pedido_pelo_processo
+            # e se addr_inicial_da_mem_livre + espaço_pedido ultrapassa o tamanho reservado na memória de 64 blocos
+            for i in range(len(rt_mem_space)):
+                if mem_block <= int(rt_mem_space[i][1]) and int(rt_mem_space[i][0]) >= 0 and int(rt_mem_space[i][0]) <= 64 and mem_block + int(rt_mem_space[i][0]) <= 64:
+                    return alloc_mem(mem_block, i, 0)
+            
+            return None
+    else:
+        # Verifica se o espaço de memória pedido foi do tamanho correto definido de no máximo 960 blocos
+        if mem_block > 960:
+            return None
+        else:
+            # Verifica se há espaço suficiente para alocar o processo. Esta verificação se dá pela conta: addr_inicial_da_mem_livre + tamanho_restante_da_mem < espaço_pedido_pelo_processo
+            # e se addr_inicial_da_mem_livre + espaço_pedido ultrapassa o tamanho reservado na memória de 960 blocos
+            for i in range(len(usr_mem_space)):
+                if mem_block <= int(usr_mem_space[i][1]) and int(usr_mem_space[i][0]) <= 1024  and mem_block + int(usr_mem_space[i][0]) <= 1024:
+                    return alloc_mem(mem_block, i, 1)
+            
+            return None
 
 # Função que aloca o processo na memória. Ela retira o espaço pedido pelo processo da lista de espaço livre.
 # Foi utilizado o first-fit, ou seja, ele aloca no primeiro espaço encontrado.
